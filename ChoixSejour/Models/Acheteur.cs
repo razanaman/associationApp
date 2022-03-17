e07d8b4847111f1636400ce8a5b4bf534438b769
razanaman <97729999+razanaman@users.noreply.github.com>
2022-03-17 18:52:57 +0100

    fix test

--- ChoixSejour/Models/Acheteur.cs
+++ ChoixSejour/Models/Acheteur.cs
@@ -46,7 +46,7 @@ namespace ChoixSejour.Models
         public string Email { get; set; }
 
         public virtual Panier Panier { get; set; }
-        // test
+        // test fix
         public virtual ICollection<Commande> Commandes { get; set; }
     }
 }
